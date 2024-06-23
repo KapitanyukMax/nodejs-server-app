f143b6baf5755248e7f3d8e9d68ae60e6837dfba
maxik20192006max@gmail.com <maxik20192006max@gmail.com>
2024-06-23 20:42:56 +0300

    middleware

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const app = express();
-
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)', (req, res) =>  {
+app.use(logger);
+
+const whitelist = ['http://127.0.0.1:5000', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/new-page(.html)?', (req, res) =>  {
+app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
 
-app.get('/old-page(.html)?', (req, res) =>  {
+app.get('/old-page(.html)?', (req, res) => {
     res.redirect(301, '/new-page.html');
 });
 
@@ -41,8 +63,17 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('/*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.sendFile({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
